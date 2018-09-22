a421fa46140e59a89972ede7949eaf8b77aff1dc
Micro-Shadi <micro.shadi@gmail.com>
2018-09-22 16:44:31 +0300

    plane

--- js/main.js
+++ js/main.js
@@ -9,6 +9,31 @@ $(function(){
   $("[data-toggle=popover]").popover();
 });
 
-/* 3D mouse movement effect */
+/* 3D mouse movement effect - parallax */
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
+var scene = document.getElementById('scene2');
+var parallaxInstance = new Parallax(scene);
+
+// Wrap every letter in a span
+$('.lettering').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+
+// scroll reveal
+ScrollReveal().reveal('.letters-animation .letter', {
+  interval: 30,
+  distance: '500px'
+});
+
+/*
+ScrollReveal().reveal('.letters-animation .letter', {
+  interval: 30,
+  distance: '50px',
+  beforeReveal: function () {
+      $('.letters-animation').addClass('letters-animate');
+  }
+});
+*/
