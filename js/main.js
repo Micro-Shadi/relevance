39983bb85640aed535f0696dcaf5297d0ffd52e2
Micro-Shadi <micro.shadi@gmail.com>
2018-09-25 14:01:05 +0300

    sticky menu

--- js/main.js
+++ js/main.js
@@ -61,3 +61,81 @@ ScrollReveal().reveal('.letters-animation .letter', {
   }
 });
 */
+
+
+
+// Sticky navbar
+// =========================
+$(document).ready(function () {
+  // Custom function which toggles between sticky class (is-sticky)
+  var stickyToggle = function (sticky, stickyWrapper, scrollElement) {
+      var stickyHeight = sticky.outerHeight();
+      var stickyTop = stickyWrapper.offset().top;
+      if (scrollElement.scrollTop() >= stickyTop) {
+          stickyWrapper.height(stickyHeight);
+          sticky.addClass("is-sticky");
+      }
+      else {
+          sticky.removeClass("is-sticky");
+          stickyWrapper.height('auto');
+      }
+  };
+
+  // Find all data-toggle="sticky-onscroll" elements
+  $('[data-toggle="sticky-onscroll"]').each(function () {
+      var sticky = $(this);
+      var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
+      sticky.before(stickyWrapper);
+      sticky.addClass('sticky');
+
+      // Scroll & resize events
+      $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function () {
+          stickyToggle(sticky, stickyWrapper, $(this));
+      });
+
+      // On page load
+      stickyToggle(sticky, stickyWrapper, $(window));
+  });
+});
+
+$(function () {
+  var lastScrollTop = 0;
+  var $navbar = $('.navbar');
+  var navbarHeight = $navbar.outerHeight();
+  var movement = 0;
+  var lastDirection = 0;
+
+  $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+    movement += st - lastScrollTop;
+
+    if (st > lastScrollTop) { // scroll down
+      if (lastDirection != 1) {
+        movement = 0;
+      }
+      var margin = Math.abs(movement);
+      if (margin > navbarHeight) {
+        margin = navbarHeight;
+      }
+      margin = -margin;
+      $navbar.css('margin-top', margin+"px")
+
+      lastDirection = 1;
+    } else { // scroll up
+      if (lastDirection != -1) {
+        movement = 0;
+      }
+      var margin = Math.abs(movement);
+      if (margin > navbarHeight) {
+        margin = navbarHeight;
+      }
+      margin = margin-navbarHeight;
+      $navbar.css('margin-top', margin+"px")
+
+      lastDirection = -1;
+    }
+
+    lastScrollTop = st;
+    // console.log(margin);
+  });
+});
