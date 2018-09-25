82086d55f9ef437f7ef49d665bde7fe182f94dd4
Micro-Shadi <micro.shadi@gmail.com>
2018-09-25 19:28:00 +0300

    car

--- js/main.js
+++ js/main.js
@@ -16,6 +16,9 @@ var parallaxInstance = new Parallax(scene);
 var scene = document.getElementById('scene2');
 var parallaxInstance = new Parallax(scene);
 
+var scene = document.getElementById('scene3');
+var parallaxInstance = new Parallax(scene);
+
 // Wrap every letter in a span
 $('.lettering').each(function(){
   $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
